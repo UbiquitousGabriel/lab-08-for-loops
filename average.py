47951fa8321d1b9dde2906c240a5e0bdb33e7dd5
Gabriel Aguilar <ubiquitousgabriel@gmail.com>
2023-10-18 21:10:02 -0400

    modified

--- average.py
+++ average.py
@@ -6,3 +6,6 @@ num4 = int(input("Please give me a number."))
 avg = (num1+num2+num3+num4/4)
 
 print(avg)
+
+for number in range(4):
+    userInp = int(input("Please enter a number"))
--- largest.py
+++ largest.py
@@ -2,7 +2,7 @@ highestNum = 0
 # initiate highestnum variable as 05
 # we will replace this 0 in our loop
 
-for i in range(0, 4, 1):
+for answer in range(0, 4, 1):
     userInput = input("Number please...")
     userInt = int(userInput)
     print("User entered:" + str(userInput))
