731f72a2a8bce8f0be7919c05f443f01e894397e
Gabriel Aguilar <ubiquitousgabriel@gmail.com>
2023-10-18 21:07:08 -0400

    Lots of things

---
+++ average.py
@@ -0,0 +1,8 @@
+num1 = int(input("Please give me a number."))
+num2 = int(input("Please give me a number."))
+num3 = int(input("Please give me a number."))
+num4 = int(input("Please give me a number."))
+
+avg = (num1+num2+num3+num4/4)
+
+print(avg)
---
+++ largest.py
@@ -0,0 +1,14 @@
+highestNum = 0
+# initiate highestnum variable as 05
+# we will replace this 0 in our loop
+
+for i in range(0, 4, 1):
+    userInput = input("Number please...")
+    userInt = int(userInput)
+    print("User entered:" + str(userInput))
+    if userInt > highestNum:
+        highestNum = userInt
+        print("Updating highest number...")
+    else:
+        print("This is not the highest number!")
+print("The highest number entered is: " + str(highestNum))
